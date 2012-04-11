16f0ebb8ccad313dbc1e85a7ec1e0b49653a071a
Anton Martynenko <amartynenko@twinfield.com>
2012-04-11 14:17:45 +0300

    Enabled support of multiple schemes

--- TestHarnessExample/Scripts/Create Database.bat
+++ TestHarnessExample/Scripts/Create Database.bat
@@ -1,5 +1,5 @@
-osql.exe -S 127.0.0.1 -U test -P test -d DataFreshSample -i database.sql
+osql.exe -S localhost -U test -P test -d DataFreshSample -i database.sql
 
-DataFreshUtil.exe -s 127.0.0.1 -u test -p test -d DataFreshSample -c PREPARE
+DataFreshUtil.exe -s localhost -u test -p test -d DataFreshSample -c PREPARE
 
 pause
